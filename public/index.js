14db155edb3adb9c34de6e38e5227ef8f1dc1c16
izuru775 <izuru775@gmail.com>
2021-11-17 02:42:14 +1100

    index.html, styles.css & index.js updated

--- public/index.js
+++ public/index.js
@@ -1,30 +1,96 @@
-// Initialize and add the map
-
-
-
-$('document').ready(function(){
-    
-    function myFunction(){
-        const lat = document.getElementById("lat");
-        console.log(lat)
-    }
-
-})
-
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+let map;
+let service;
+let infowindow;
 
 function initMap() {
-    // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.036 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: uluru,
-    });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
+  const origin = { lat: -37.847, lng: 145.115 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 18,
+    center: origin,
+  });
+
+  new ClickEventHandler(map, origin);
+}
+
+function isIconMouseEvent(e) {
+  return "placeId" in e;
+}
+
+class ClickEventHandler {
+  origin;
+  map;
+  directionsService;
+  directionsRenderer;
+  placesService;
+  infowindow;
+  infowindowContent;
+  constructor(map, origin) {
+    this.origin = origin;
+    this.map = map;
+    this.directionsService = new google.maps.DirectionsService();
+    this.directionsRenderer = new google.maps.DirectionsRenderer();
+    this.directionsRenderer.setMap(map);
+    this.placesService = new google.maps.places.PlacesService(map);
+    this.infowindow = new google.maps.InfoWindow();
+    this.infowindowContent = document.getElementById("infowindow-content");
+    this.infowindow.setContent(this.infowindowContent);
+    // Listen for clicks on the map.
+    this.map.addListener("click", this.handleClick.bind(this));
+  }
+  handleClick(event) {
+    console.log("You clicked on: " + event.latLng);
+    // If the event has a placeId, use it.
+    if (isIconMouseEvent(event)) {
+      console.log("You clicked on place:" + event.placeId);
+      // Calling e.stop() on the event prevents the default info window from
+      // showing.
+      // If you call stop here when there is no placeId you will prevent some
+      // other map click event handlers from receiving the event.
+      event.stop();
+      if (event.placeId) {
+        this.calculateAndDisplayRoute(event.placeId);
+        this.getPlaceInformation(event.placeId);
+      }
+    }
+  }
+  calculateAndDisplayRoute(placeId) {
+    const me = this;
+
+    this.directionsService
+      .route({
+        origin: this.origin,
+        destination: { placeId: placeId },
+        travelMode: google.maps.TravelMode.WALKING,
+      })
+      .then((response) => {
+        me.directionsRenderer.setDirections(response);
+      })
+      .catch((e) => window.alert("Directions request failed due to " + status));
+  }
+  getPlaceInformation(placeId) {
+    const me = this;
+
+    this.placesService.getDetails({ placeId: placeId }, (place, status) => {
+      if (
+        status === "OK" &&
+        place &&
+        place.geometry &&
+        place.geometry.location
+      ) {
+        me.infowindow.close();
+        me.infowindow.setPosition(place.geometry.location);
+        me.infowindowContent.children["place-icon"].src = place.icon;
+        me.infowindowContent.children["place-name"].textContent = place.name;
+        me.infowindowContent.children["place-id"].textContent = place.place_id;
+        me.infowindowContent.children["place-address"].textContent =
+          place.formatted_address;
+        me.infowindow.open(me.map);
+      }
     });
   }
-
-
+}
+// -37.847107535405485, 145.1151394021206 (Deakin)
+// -37.85973823884486, 145.25335059837852 (Sodi-home)
