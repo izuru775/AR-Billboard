98699d16686cb7e2cf45707e930c6b17e4b1a40c
AndrewJ1993 <andrew.s.jarrett1@gmail.com>
2021-11-16 20:10:55 +1100

    Converted project to use react. Put API key in .env file. Added GPS location to app.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# sit725-prac3
+React App with Google maps API created with the help of:
+https://www.digitalocean.com/community/tutorials/how-to-integrate-the-google-maps-api-into-react-applications
--- server.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-let app =new express();
-//sewrve static web page
-app.use(express.static("public"));
-
-
-app.listen(3000,function(){
-    console.log("Server start on port 3000")
-});
